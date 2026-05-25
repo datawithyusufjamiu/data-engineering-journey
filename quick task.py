5975e08d65e6312141ec616036d4e41776ac65a1
Yusuf Jamiu <datawithyusufjamiu@email.com>
2026-05-25 10:47:10 -0700

    Day 2 understanding - lists, loops, enumerate from memory

---
+++ quick task.py
@@ -0,0 +1,26 @@
+#This is a variable
+
+street_name = "tanke"
+street_name = "okeodo"
+street_name = "gaa aknabi"
+street_name = "agba"
+
+#Thia is a list
+states = [ "Lagos","Abuja", "Kano", "Rivers"]
+
+#This is a loop
+for index, state in enumerate(states):
+    print(f"{index + 1}. {state}")
+
+my_set = {"Lagos", "Abuja"}
+
+
+foods = ["Rice", "Beans", "Bread", "Garri", "Akara"]
+
+for index,food in enumerate (foods):
+    print(f"{index + 1}. {food}")
+
+players = ["Ronaldo", "Messi", "Salah", "Osimhen", "Mbappe"]
+
+for index, player in enumerate (players):
+    print(f"{index+1}. {player}")
